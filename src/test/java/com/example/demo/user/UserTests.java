a01990d84d549a5289f567ea571a26fc9ef8c24b
Melchior Lozé <melchior.loze@gmail.com>
2024-05-09 22:04:51 +0200

    add test for User.toString() and User.hashCode()

--- src/test/java/com/example/demo/user/UserTests.java
+++ src/test/java/com/example/demo/user/UserTests.java
@@ -44,4 +44,21 @@ class UserTests {
         assertThrows(TransactionSystemException.class,
                 () -> userRepository.save(new User("John Doe", "john doe@example.com")));
     }
+
+    @Test
+    void shouldReturnUserAsString() {
+        String name = "John Doe";
+        String email = "john.doe@example.com";
+        User user = new User(name, email);
+        assertEquals(String.format("User(id=null, name=%s, email=%s)", name, email), user.toString());
+    }
+
+    @Test
+    void shouldReturnUserHashCode() {
+        String name = "John Doe";
+        String email = "john.doe@example.com";
+        User user1 = new User(name, email);
+        User user2 = new User(name, email);
+        assertEquals(user1.hashCode(), user2.hashCode());
+    }
 }
